3d35ba5469f9cecfce02cc6152b020f9ae10de8e
Phạm Phước Bảo Tín <baotinphamphuoc@gmail.com>
2023-04-25 23:17:39 +0700

    Update sap_xep.py

--- sap_xep.py
+++ sap_xep.py
@@ -24,12 +24,15 @@ def sap_xep_noi(lst):
 def sap_xep_chen(lst):
     lst_1=lst.copy()
     for i in range(len(lst)):
+        # 0,1,2
+        # 1,4,3
         index=i
         value=lst_1[i]
         while( index>0 and(lst_1[index-1]>value)):
             lst_1[index]=lst[index-1]
             index-=1
         lst_1[index]=value
+        print(lst_1[index])
     return lst_1
 
-print(sap_xep_chen([2,1,5]))
+print(sap_xep_chen([1,4,3]))
