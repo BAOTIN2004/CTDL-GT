fd48fb75dbd84d78d0a9fc49efc9ff784b890e78
Phạm Phước Bảo Tín <baotinphamphuoc@gmail.com>
2023-05-08 23:37:43 +0700

    sua

--- sap_xep.py
+++ sap_xep.py
@@ -52,4 +52,24 @@ def sap_xep_nhanh(lst):
   #  lst_1[left],lst_1[right]=lst_1[right],lst_1[left]
     return lst_1
 
-print(sap_xep_nhanh([2,3,1,4]))
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quicksort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quicksort(arr, low, pi - 1)
+        quicksort(arr, pi + 1, high)
+
+# Sử dụng hàm quicksort
+arr = [3, 1, 7, 5, 2, 4, 6]
+quicksort(arr, 0, len(arr) - 1)
+print(arr)
+
--- tháp hà nội.py
+++ tháp hà nội.py
@@ -12,11 +12,16 @@ def ucln1(a, b):
 
 def thap_ha_noi(n,cot_a,cot_b,cot_c):
     if n==1: 
-        print(f'Chuyển đĩa {n} từ {cot_a} sang {cot_c}  ')
+        print(f'Chuyển đĩa 1 từ {cot_a} sang {cot_c}  ')
         return
     thap_ha_noi(n-1,cot_a,cot_c,cot_b)
     print(f'Chuyển đĩa {n} từ { cot_a} sang {cot_c} ')      
     thap_ha_noi(n-1,cot_b,cot_a,cot_c)
+thap_ha_noi(3,'A',"B","C")
 
-        
-        
+# Chuyển đĩa 1 từ A sang C
+# Chuyển đĩa 2 từ A sang B
+# Chuyển đĩa 1 từ C sang B
+# Chuyển đĩa 3 từ A sang C
+# Chuyển đĩa 1 từ B sang A
+# Chuyển đĩa 1 từ A sang C
